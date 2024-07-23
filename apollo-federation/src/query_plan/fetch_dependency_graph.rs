bf3db2c9a81a240c457094950bdc2d1d6a3ec713
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2024-07-22 22:39:18 -0500

    fix key for subgraph key fetch

--- apollo-federation/src/query_plan/fetch_dependency_graph.rs
+++ apollo-federation/src/query_plan/fetch_dependency_graph.rs
@@ -2516,7 +2516,9 @@ impl FetchDependencyGraphNode {
         // hasInputs ? `${toValidGraphQLName(subgraphName)}-${mergeAt?.join('::') ?? ''}` : undefined,
         // ```
         // TODO: We could use a numeric hash key in Rust, instead of a string key as done in JS.
-        self.inputs.as_ref()?;
+        if self.inputs.is_none() {
+            return None;
+        }
         let subgraph_name = &self.subgraph_name;
         let merge_at_str = match self.merge_at {
             Some(ref merge_at) => merge_at
